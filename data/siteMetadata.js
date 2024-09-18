367a6a5fb4998623d0dba67e40cefe55d76ef5c7
cby <394220838@qq.com>
2024-09-18 10:32:24 +0800

    feat: update blog content

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -6,7 +6,7 @@ const siteMetadata = {
   description: 'AI与科技的探索',
   language: 'en-us',
   theme: 'system', // system, dark or light
-  siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
+  siteUrl: 'https://blog.ainfo.live',
   siteRepo: 'https://github.com/SXGC/nextjs-blog.git',
   siteLogo: `${process.env.BASE_PATH || ''}/static/images/logo.png`,
   socialBanner: `${process.env.BASE_PATH || ''}/static/images/twitter-card.png`,
